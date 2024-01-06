32584e684db48f45ef12b4a29d12de8b93f8063e
naixsu <plnacua02@gmail.com>
2024-01-06 18:50:54 +0800

    Basic animation

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -6,17 +6,32 @@ extends CharacterBody2D
 # following tutorial 
 # https://www.youtube.com/watch?v=mAbG8Oi-SvQ&list=PL9FzW-m48fn2SlrW0KoLT4n5egNdX-W9a
 
+# Signals
+# Enums
+# Constants
 const SPEED : float = 60
 const ACCELERATION : float = 80
 const FRICTION : float = 2000
 
+# Export Variables
+# Public Variables
+# Private Variables
+var direction : Vector2 = Vector2.ZERO
+# Onready variables
+@onready var animation_player = $Animations/AnimationPlayer
+
+
+
+
+
 func _physics_process(delta):
 	move_player(delta)
+	update_animation()
 
 
 #region General Methods
 func move_player(delta) -> void:
-	var direction = Input.get_vector("Left", "Right", "Up", "Down") # already normalized
+	direction = Input.get_vector("Left", "Right", "Up", "Down") # already normalized
 	# velocity = direction * SPEED * ACCELERATION * delta
 	
 	# might not need the if-else below
@@ -38,5 +53,19 @@ func move_player(delta) -> void:
 		#velocity = velocity.limit_length(SPEED * delta)
 	#else:
 		#velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
+
+func update_animation() -> void:
+	if velocity != Vector2.ZERO:
+		if direction.x > 0:
+			animation_player.play("RunRight")
+		elif direction.x < 0:
+			animation_player.play("RunLeft")
+		elif direction.y < 0:
+			animation_player.play("RunUp")
+		elif direction.y > 0:
+			animation_player.play("RunDown")
+	else:
+		animation_player.play("IdleRight")
 
 #endregion
